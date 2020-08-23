28bf3f1667069454ab4fedd8ce7c72b9017f1e5f
Dionyssios Frangopoulos <e@dnf.gr>
2020-08-23 16:20:29 +0300

    fixes

--- 0toxfce.sh
+++ 0toxfce.sh
@@ -40,6 +40,6 @@ echo "--> Getting Packages"
 
 apt -y update
 apt -y upgrade
-apt -y install xfce4 moka-icon-theme arc-theme thunderbird xfce4-terminal firefox-esr network-manager-gnome keepassxc gimp build-essential wget curl findutils synaptic firmware-linux firmware-iwlwifi pulseaudio xfce4-power-manager
+apt -y install xfce4 moka-icon-theme arc-theme lm-sensors mousepad netcat thunderbird xfce4-terminal firefox-esr network-manager-gnome keepassxc gimp build-essential wget curl findutils synaptic firmware-linux firmware-iwlwifi pulseaudio xfce4-power-manager
 
 tasksel install laptop
