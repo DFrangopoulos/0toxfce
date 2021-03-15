34292f42bde6b4eb60b141b89cbf89b48e635307
df <df@df.df>
2021-03-15 18:42:19 +0200

    Fixed Script

--- 0toxfce.sh
+++ 0toxfce.sh
@@ -16,11 +16,21 @@ cat << EOF
 
 EOF
 #####
+
+#Check UID
+userid=$(id -u)
+echo $userid
+
+if [ $userid -ne 0 ]
+then
+        echo "Not running as root!Exiting..."
+        exit 1
+fi
+
 echo "--> Populating APT Sources"
 
 > /etc/apt/sources.list
 cat << 'EOF' >> /etc/apt/sources.list
-
 
 deb http://deb.debian.org/debian/ buster main contrib non-free
 deb-src http://deb.debian.org/debian/ buster main contrib non-free
@@ -33,13 +43,14 @@ deb-src http://deb.debian.org/debian/ buster-updates main contrib non-free
 
 EOF
 
-#test "$?" && echo "Permission Error!" && exit
+#test "$?" && echo "Failed to modify sources!" && exit
 
 #####
 echo "--> Getting Packages"
 
 apt -y update
 apt -y upgrade
-apt -y install xfce4 moka-icon-theme arc-theme lm-sensors mousepad netcat thunderbird xfce4-terminal firefox-esr network-manager-gnome keepassxc gimp build-essential wget curl findutils synaptic firmware-linux firmware-iwlwifi pulseaudio xfce4-power-manager
+apt -y install --no-install-recommends xfce4
+apt -y install xinit xorg xfce4-terminal pavucontrol slock conky moka-icon-theme arc-theme build-essential keepassxc wget curl netstat htop
 
-tasksel install laptop
+exit 0
